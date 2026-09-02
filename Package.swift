1759d90362a41f7193f8e6e003b3f9f9c176a181
Jorge Poveda <jpoveda@facephi.com>
2026-09-02 12:17:59 +0200

    fix: raise iOS min to 13.0 and bump Selphi widget to 6.3.2 (EST-667)
    Align Package platforms with Selphi-SPM/IAD 6.3.2 requirement.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SDK-Selphi_component-SPM",
     defaultLocalization: "en",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", .exactItem("2.10.0")),
-        .package(url: "https://github.com/facephi-clienters/Selphi-IAD-SPM.git", .upToNextMinor(from: "6.3.0"))
+        .package(url: "https://github.com/facephi-clienters/Selphi-IAD-SPM.git", .upToNextMinor(from: "6.3.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
